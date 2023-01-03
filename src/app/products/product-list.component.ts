371f31ebedf1d32667c35c112776e4d433e6a91e
Kinjalben Goalviya <Kinjalben.Goalviya@exxeta.com>
2023-01-03 17:33:11 +0100

    getter and setter

--- src/app/products/product-list.component.ts
+++ src/app/products/product-list.component.ts
@@ -10,7 +10,16 @@ export class ProductListComponent implements OnInit {
   imageWidth: number = 50;
   imageMargin: number = 2;
   showImage: boolean = false;
-  listFilter: string = 'cart';
+  private _listFilter: string = '';
+  get llistFilter(): string {
+    return this._listFilter;
+  }
+
+  set listFilter(value: string) {
+    this._listFilter = value;
+    console.log('setter:', value);
+  }
+
   products: IProduct[] = [
     {
       productId: 1,
