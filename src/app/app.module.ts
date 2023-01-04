166802c387c6534b2f539e5dbf6bc1dd22eb2c5f
Kinjalben Goalviya <Kinjalben.Goalviya@exxeta.com>
2023-01-04 10:44:43 +0100

    build and use nested component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,15 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { ProductListComponent } from './products/product-list.component';
 import { ConvertToSpacePipe } from './shared/convert-to-spaces.pipe';
+import { starComponent } from './shared/star.component';
 
 @NgModule({
-  declarations: [AppComponent, ProductListComponent, ConvertToSpacePipe],
+  declarations: [
+    AppComponent,
+    ProductListComponent,
+    ConvertToSpacePipe,
+    starComponent,
+  ],
   bootstrap: [AppComponent],
   imports: [BrowserModule, FormsModule],
 })
