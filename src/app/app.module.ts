bf39609d11f82a0bf9c57696d50be73511c6c111
Kinjalben Golaviya <Kinjalben.golaviya@exxeta.com>
2023-01-20 18:47:09 +0100

    feature & shared module added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,42 +1,24 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
-import { ProductListComponent } from './products/product-list.component';
-import { ConvertToSpacePipe } from './shared/convert-to-spaces.pipe';
-import { starComponent } from './shared/star.component';
-import { ProductDetailComponent } from './products/product-detail.component';
 import { WelcomeComponent } from './home/welcome.component';
-import { RouterModule } from '@angular/router'; 
-import { ProductDetailGuard } from './products/product-detail.guard';
+import { RouterModule } from '@angular/router';
+import { ProductModule } from './products/product.module';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    WelcomeComponent,
-    ProductListComponent,
-    ConvertToSpacePipe,
-    starComponent,
-    ProductDetailComponent,
-  ],
+  declarations: [AppComponent, WelcomeComponent],
   bootstrap: [AppComponent],
   imports: [
     BrowserModule,
-    FormsModule,
     HttpClientModule,
     RouterModule.forRoot([
-      { path: 'products', component: ProductListComponent },
-      {
-        path: 'products/:id',
-        canActivate: [ProductDetailGuard],
-        component: ProductDetailComponent,
-      },
       { path: 'welcome', component: WelcomeComponent },
       { path: ' ', redirectTo: 'welcome', pathMatch: 'full' },
       { path: '**', redirectTo: 'welcome', pathMatch: 'full' },
     ]),
+    ProductModule,
   ],
 })
 export class AppModule {}
